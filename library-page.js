9fcc22a7cbd0388b78a3910755d1bae1c56e9127
pandorafromtheothers <115832798+pandorafromtheothers@users.noreply.github.com>
2026-08-22 01:33:35 +0200

    url compatible convert, extend manifest

--- library-page.js
+++ library-page.js
@@ -4,7 +4,7 @@ let searchLink = window.innerWidth > 600 ? "https://music.youtube.com/search?q="
 
 for (let index = 0; index < array.length; index++) {
     let element = array[index];
-    artist = element.querySelector(".artistTitle").innerText;
-    album = element.querySelector(".albumTitle").innerText;
+    artist = encodeURIComponent(element.querySelector(".artistTitle").innerText);
+    album = encodeURIComponent(element.querySelector(".albumTitle").innerText);
     element.querySelector(".type.functions").innerHTML += `<a href=\"${searchLink}${artist} ${album}\" target=\"_blank\"><div class=\"youtube\"><i class=\"fab fa-youtube\"></i></div></a>`;
 }
